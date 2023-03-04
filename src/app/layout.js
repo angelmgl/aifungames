1bdc824b508ec26c2ef534d0d9eb92293612fd5e
Angel Alemany <angelemegeele@gmail.com>
2023-03-04 17:19:55 -0300

    Add credits

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,7 +15,19 @@ const font = Alegreya_Sans({
 export default function RootLayout({ children }) {
     return (
         <html lang="es">
-            <body className={font.className + " bg-gray-800"}>{children}</body>
+            <body className={font.className + " bg-gray-800"}>
+                <span className="inline-block pl-8 pt-2 text-white absolute">
+                    Desarrollado por{" "}
+                    <a
+                        className="text-cyan-300 hover:text-cyan-500 hover:underline"
+                        href="https://twitter.com/alemanydev"
+                        target="_blank"
+                    >
+                        @alemanydev
+                    </a>
+                </span>
+                {children}
+            </body>
         </html>
     );
 }
